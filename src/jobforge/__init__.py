6f85013fdd818046783a2d6c0b9df50c17f0f117
pavankumarhfl-hub <pavankumar.hfl@gmail.com>
2026-09-07 21:34:08 +0530

    feat: expose worker API and version 0.2

--- src/jobforge/__init__.py
+++ src/jobforge/__init__.py
@@ -1,6 +1,7 @@
 """JobForge: a small durable SQLite job queue."""
 
 from .queue import Job, Queue
+from .worker import run_worker
 
-__all__ = ["Job", "Queue"]
-__version__ = "0.1.0"
+__all__ = ["Job", "Queue", "run_worker"]
+__version__ = "0.2.0"
